52914cdd2a39f79fa0d90295a548d68633921f57
Weldan Jamili <mweldan@gmail.com>
2013-05-10 05:47:28 +0800

    show hello world

--- sokoban/windows.py
+++ sokoban/windows.py
@@ -7,6 +7,13 @@ class sokoban_windows(object):
         
     def create(self):
         window = pygame.display
-        window.set_mode(self.size).convert_alpha()
+        screen = window.set_mode(self.size, 0, 24)
+        window.get_surface().fill((255, 255, 255))
+        
+        # test show text 
+        font = pygame.font.SysFont("comicsansms", 30)
+        text = font.render("Hello World", True, (0, 0, 0), (255, 255, 255))
+        screen.blit(text, (300, 250))
+        
         window.set_caption(self.caption) 
         window.update()
